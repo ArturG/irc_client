49c8b242e3a5dbe25b78f9e1a2ec22452f0bd4dd
Artur Gaifutdinov <Rix@Arturs-MacBook-Air.local>
2014-02-02 13:10:15 -0500

    Refactoring

--- irc.c
+++ irc.c
@@ -7,40 +7,49 @@
 #include <unistd.h>
 #include <pthread.h> 
 
-void* receive_message(void* arg) {
-  int sockfd = *((int *) arg);
-  char buffer[512];
-  char *chan = "#testmeout";
+#define IRC_CHANNEL "#help"
+#define IRC_ADDRESS "irc.quakenet.org"
+#define OWNER "Bingo"
+#define NICKNAME "SuchImpressive"
 
-  
-  while (recv(sockfd, buffer, 512, 0) > 0) {
-    fputs(buffer, stdout);
+void* receive_message(void* arg) {
+  int irc_socket = *((int *) arg);
+  char buffer[512];
+
+  while (recv(irc_socket, buffer, 512, 0) > 0) {
+    //fputs(buffer, stdout);
     
     if (!strncmp(buffer, "PING ", 5)) {
-      buffer[1] = 'O';
-      send(sockfd, buffer, strlen(buffer), 0);
+      buffer[1] = 'O'; //replace char 'I' on 'O' inside 'PING'
+      send(irc_socket, buffer, strlen(buffer), 0); //send 'PONG'
+    }
+
+    if (strstr(buffer, "PRIVMSG #") != NULL) {
+      printf("Received message: %s\n", buffer);
     }
 
     if (!strncmp(strchr(buffer, ' ') + 1, "001", 3)) {
-      sprintf(buffer, "JOIN %s\r\n", chan);
-      send(sockfd, buffer, strlen(buffer), 0);
+      sprintf(buffer, "JOIN %s\r\n", IRC_CHANNEL);
+      send(irc_socket, buffer, strlen(buffer), 0);
+      printf(".:. Connected to the channel %s .:. \n", IRC_CHANNEL);
+      printf(".:. You can send messages directly through the console .:. \n");
     }
   }
 }
 
 void* send_message(void* arg) {
-  int sockfd = *((int *) arg);
+  int irc_socket = *((int *) arg);
   char buffer[512];
   char message[512];
   
   if (fgets(message, sizeof message, stdin)) {
-    sprintf(buffer, "PRIVMSG #testmeout :%s\r\n", message);
-    send(sockfd, buffer, strlen(buffer), 0);  
-    printf("%s\n", buffer);
+    sprintf(buffer, "PRIVMSG %s :%s\r\n", IRC_CHANNEL, message);
+    send(irc_socket, buffer, strlen(buffer), 0);  
+    printf("You said: %s\n", message);
   }  
 }
 
-int connect_to_server(char *address, char *owner, char *nickname) {
+int connect_to_server() {
   struct addrinfo hints;
   struct addrinfo *res;
   
@@ -48,19 +57,19 @@ int connect_to_server(char *address, char *owner, char *nickname) {
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   
-  getaddrinfo(address, "6667", &hints, &res);
+  getaddrinfo(IRC_ADDRESS, "6667", &hints, &res);
   
-  int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  connect(sock, res->ai_addr, res->ai_addrlen);
+  int irc_socket = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
+  connect(irc_socket, res -> ai_addr, res -> ai_addrlen);
 
   char buffer[512];
   
-  sprintf(buffer, "USER %s 0 * :%s\r\n", nickname, owner);
-  send(sock, buffer, strlen(buffer), 0);
-  sprintf(buffer, "NICK %s\r\n", nickname);
-  send(sock, buffer, strlen(buffer), 0);
+  sprintf(buffer, "USER %s 0 * :%s\r\n", NICKNAME, OWNER);
+  send(irc_socket, buffer, strlen(buffer), 0);
+  sprintf(buffer, "NICK %s\r\n", NICKNAME);
+  send(irc_socket, buffer, strlen(buffer), 0);
   
-  return sock;
+  return irc_socket;
 }
 
 
@@ -68,10 +77,10 @@ int main(int argc, char *argv[]) {
   pthread_t receiver;
   pthread_t sender;
 
-  int sock = connect_to_server(argv[1], argv[2], argv[3]);
+  int irc_socket = connect_to_server();
 
   while (1) {
-    pthread_create (&receiver, NULL, &receive_message, &sock);
-    pthread_create (&sender, NULL, &send_message, &sock);
+    pthread_create (&receiver, NULL, &receive_message, &irc_socket);
+    pthread_create (&sender, NULL, &send_message, &irc_socket);
   }
 }
