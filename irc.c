729c4bd46561c47f17611bfe60edebdf2fff5725
Artur-Gaifutdinov <invictusru@gmail.com>
2014-02-23 20:19:04 -0500

    Reconnection enabled

--- irc.c
+++ irc.c
@@ -13,15 +13,39 @@
 #define IRC_PORT "6667"
 #define OWNER "Bingo"
 #define NICKNAME "SomeRandomNickname"
-
-/* Defines time in which IRC client will timeout 
- * if no data is received */
 #define NO_DATA_TIMEOUT 300
-#define RECONNECT_INTERVAL 60
+#define RECONNECT_INTERVAL 30
+
+#define STARTING 0
+#define WORKING 1
+#define RESTARTING 2
 
 time_t IRC_LAST_ACTIVITY = 0;   // time of receiving last full line data from IRC server
+time_t IRC_RECONNECTION_ACTIVITY = 0; // time of latest reconnection attempt
 
-void* receive_message(void* arg) {
+
+int con_socket;
+
+int status = STARTING;
+
+
+void irc_reconnect()
+{
+  time_t present, delta;   
+  time(&present);
+  
+  delta = present - IRC_RECONNECTION_ACTIVITY;
+
+  if (delta >= RECONNECT_INTERVAL) {
+    printf("IRC -> Connection to (%s) failed, reconnecting. \n", IRC_ADDRESS);
+
+    status = RESTARTING;
+    time(&IRC_RECONNECTION_ACTIVITY);
+  }
+}
+
+void* receive_message(void* arg)
+{
   int irc_socket = *((int *) arg);
   char buffer[512];
   
@@ -46,9 +70,13 @@ void* receive_message(void* arg) {
 
     time(&IRC_LAST_ACTIVITY);
   }
+
+  // if while loop finished it means that we lost connection
+  irc_reconnect();
 }
 
-void* send_message(void* arg) {
+void* send_message(void* arg)
+{
   int irc_socket = *((int *) arg);
   char buffer[512];
   char message[512];
@@ -60,18 +88,8 @@ void* send_message(void* arg) {
   }  
 }
 
-
-void irc_reconnect() {
-  time_t present;
-   
-  time(&present);
-  
-  printf("IRC -> Connection to (%s) failed, reconnecting. \n", IRC_ADDRESS);
-
-  //TODO
-}
-
-void* irc_timer() {
+void* irc_timer()
+{
   sleep(30);
 
   time_t present, delta;
@@ -86,71 +104,88 @@ void* irc_timer() {
   }
 }
 
-int irc_login(socket) {
+int irc_login(socket)
+{
   char buffer[512];
-  //this block should be moved from here
   sprintf(buffer, "USER %s 0 * :%s\r\n", NICKNAME, OWNER);
   send(socket, buffer, strlen(buffer), 0);
   sprintf(buffer, "NICK %s\r\n", NICKNAME);
   send(socket, buffer, strlen(buffer), 0);    
 }
 
-int get_socket(char* host, char* port) {
+int get_socket(char* host, char* port)
+{
   int rc;
-  int irc_socket;
+  int irc_socket=-1;
   struct addrinfo hints;
-  struct addrinfo *res;
+  struct addrinfo *res=NULL;
   
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   
-  if ((rc = getaddrinfo(host, port, &hints, &res)) != 0 ) {
-    fprintf(stderr, "getaddrinfo() error: %s\n", gai_strerror(rc));
-    return -1;
-  }
+  if ((rc = getaddrinfo(host, port, &hints, &res)) == 0 ) 
+    {
 
-  irc_socket = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
-  
-  if (irc_socket < 0) {
-    fprintf(stderr, "Couldn't get socket.\n");
-    goto error;
-  }
+     if ((irc_socket = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol)) > 0) 
+       {
 
-  if (connect(irc_socket, res -> ai_addr, res -> ai_addrlen) < 0) {
-    fprintf(stderr, "Couldn't connect.\n");
-    goto error;
-  }
-  
-  freeaddrinfo(res);
+        if (connect(irc_socket, res -> ai_addr, res -> ai_addrlen) < 0)
+         {
+           fprintf(stderr, "Couldn't connect.\n");
+           close(irc_socket);
+           irc_socket=-1;
+         }
+       }
+     else fprintf(stderr, "Couldn't get socket.\n");
+    }
+   
+  else fprintf(stderr, "getaddrinfo() error: %s\n", gai_strerror(rc));
+
+  if (res) freeaddrinfo(res);
   return irc_socket;
+}
 
-error:
-  freeaddrinfo(res);
-  return -1;
+int irc_cycle()
+{
+  con_socket = get_socket(IRC_ADDRESS, IRC_PORT);
+  
+  if (con_socket < 0 ) {
+    printf("Connection failed.\n");
+    status = RESTARTING;
+  } 
+  
+  sleep(5);
+  irc_login(con_socket);
 }
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   pthread_t receiver;
   pthread_t sender;
   pthread_t timer;
   
-  int irc_socket = get_socket(IRC_ADDRESS, IRC_PORT);
-
-  if (irc_socket < 0 ) {
-    printf("Connection failed.\n");
-    goto exit_err;
-  } 
-
-  irc_login(irc_socket);
-  
   while (1) {
-    pthread_create (&receiver, NULL, &receive_message, &irc_socket);
-    pthread_create (&sender, NULL, &send_message, &irc_socket);
-    pthread_create (&timer, NULL, &irc_timer, NULL); 
-  }
 
-exit_err:
-  exit(1);
+    if (status == STARTING) {
+      irc_cycle();
+      status = WORKING;
+    }
+    
+    if (status == WORKING) {
+      pthread_create (&receiver, NULL, &receive_message, &con_socket);
+      pthread_create (&sender, NULL, &send_message, &con_socket);
+      pthread_create (&timer, NULL, &irc_timer, NULL); 
+    }
+
+    if (status == RESTARTING) {
+      pthread_cancel(receiver);
+      pthread_cancel(sender);
+      pthread_cancel(timer);
+
+      status = STARTING;
+      close(con_socket);
+    }
+  }
 }
